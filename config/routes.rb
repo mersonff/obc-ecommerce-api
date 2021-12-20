40a5b6020fa6d722d6ad08b6ed68aff88c510de0
Emerson Freitas <mersonsi@gmail.com>
2021-12-20 19:18:56 -0300

    setup: dockerizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
+  mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
