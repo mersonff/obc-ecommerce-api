be36acacee75ad439dcc1dab78247777c3dd9c63
Emerson Freitas <mersonsi@gmail.com>
2021-12-21 20:07:42 -0300

    setup: devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
+
+  namespace :admin do
+    namespace :v1 do
+      get "home" => "home#index"
+    end
+  end
+
+  namespace :storefront do
+    namespace :v1 do
+
+    end
+  end
+
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
