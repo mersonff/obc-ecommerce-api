5d5025c2efbca4115d0b21b04cd5625e49c0d191
Emerson Freitas <mersonsi@gmail.com>
2022-01-04 20:44:58 -0300

    feat(shared examples): create

--- spec/requests/admin/v1/categories/admin_requests_spec.rb
+++ spec/requests/admin/v1/categories/admin_requests_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::V1::Categories", type: :request do
+RSpec.describe "Admin V1 Categories as :admin", type: :request do
   describe "GET /index" do
     let(:user) { create(:user) }
 
